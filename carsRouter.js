33c3f6f98e7a50b4f6979fc83a5d0fdc59b71d56
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-17 22:15:52 -0500

    router for cars created. GET/POST routes working

--- carsRouter.js
+++ carsRouter.js
@@ -1,1 +1,36 @@
 //router for cars data
+const express = require('express');
+const knex = require('knex');
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './data/car-dealer.db3'
+  },
+  useNullAsDefault: true
+});
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error:"could not get cars"})
+        })
+})
+
+router.post('/', (req, res) => {
+    db('cars').insert(req.body)
+        .then(adding => {
+            res.status(201).json('adding car with id '+ adding)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "could not get cars" })
+        })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 //cars server
 const express = require('express'); //import express
 const server = express(); //use express as server
+const CarsRouter = require('./carsRouter');
 
 server.use(express.json());
 
 //import cars router eventually and use
+
+server.use('/cars', CarsRouter);
 
 server.get('/', (req, res) => { //base route
     res.send('initial server start')
